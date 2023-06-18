0ab4822aa3e2e485024060fad9e2103f18b5589b
Krishnendu <kd.datascience@gmail.com>
2023-06-18 17:40:34 +0530

    record text input

--- static/scripts/recordTextInput.js
+++ static/scripts/recordTextInput.js
@@ -1,29 +1,50 @@
 //function to display text message 
 function sendMessage() {
     const userInput = document.getElementById('textInput').value.trim();
-    if (userInput.trim() !== '') {
-        const userMessage = createUserMessage(userInput);
-        document.getElementById('chatWindow').appendChild(userMessage);
-        document.getElementById('textInput').value = '';
+    const userMessage = createUserMessage(userInput);
+    document.getElementById('chatWindow').appendChild(userMessage);
+    document.getElementById('textInput').value = '';
+    if (userInput !== '') {
+        fetch('/text-question', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ text: userInput })
+          }).then(response => response.json())
+          .then(responseData => {
+        console.log("Response from backend:", responseData);
 
         // Optionally, you can send the user's text message to your chatbot backend for processing
         // In this example, we'll simulate a response from the bot
-        simulateBotTextResponse();
-    }
-}
+        simulateBotTextResponse(responseData);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        // Handle any errors that occur during the request
+      });
+}}
 
 //Function to create a user message
 function createUserMessage(message) {
     const userMessage = document.createElement('div');
     userMessage.className = 'userMessage';
     userMessage.innerHTML ="<p style='text-align:right;'>" + message + "</p>";
+    // console.log(message);
+    // $.ajax({
+    //     type: 'POST',
+    //     url: '/text-question',
+    //     data: JSON.stringify({'input_text':message}),
+    //     contentType: false,
+    //     processData: false
+    // });
     return userMessage;
   }
 
-function simulateBotTextResponse(){
+function simulateBotTextResponse(openai_response){
     const botMessage = document.createElement('div');
     botMessage.className = 'botMessage';
-    botMessage.innerHTML ="<p>" + "OpenAI Response" + "</p>";
+    botMessage.innerHTML ="<p>" + openai_response + "</p>";
     return document.getElementById('chatWindow').appendChild(botMessage);
 
     }
