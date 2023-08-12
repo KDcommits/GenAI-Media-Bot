f71bedf5c99eb20921cc005cb34fd4642395030e
Krishnendu <kd.datascience@gmail.com>
2023-08-12 15:50:52 +0530

    UI modification

--- static/scripts/toggleChatbot.js
+++ static/scripts/toggleChatbot.js
@@ -2,17 +2,11 @@ let isExpanded = false;
 
 function toggleChatbot() {
   const chatbotWindow = document.getElementById('chatbotWindow');
-  const chatbotContent = document.getElementById('chatWindow');
-  const buttonbotContent = document.getElementById('buttonWindow');
 
   if (isExpanded) {
     chatbotWindow.classList.remove('expanded');
-    // chatbotContent.style.display = 'none';
-    // buttonbotContent.style.display = 'none';
   } else {
     chatbotWindow.classList.add('expanded');
-    // chatbotContent.style.display = 'block';
-    // buttonbotContent.style.display = 'block';
   }
 
   isExpanded = !isExpanded;
