2deed49ec35c3657913340ab7a87930546c2ce57
Krishnendu <kd.datascience@gmail.com>
2023-10-04 01:16:33 +0530

    modifications

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ import os
 import re
 import json 
 import glob
+import time
 import openai
 import shutil
 import datetime
 import pandas as pd
 from dotenv import load_dotenv
 from transcript import speech2Text
-from data import Data
-from model import Model
+from data_final import Data
+from model_final import Model
 from excelAI import ExcelQuery
 from flask import Flask,request, jsonify, render_template
 
 load_dotenv()
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_KEY')
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 OPENAI_KEY = os.getenv("OPENAI_KEY")
 
 
@@ -25,26 +25,41 @@ def filename():
 
 
 app= Flask(__name__)
-app.config["UPLOAD_AUDIO_FOLDER"] ="./Data/Input/Input Audio/"
-app.config["UPLOAD_PDF_FOLDER"] ="./Data/Input/Input File/"
-app.config["UPLOAD_EXCEL_FOLDER"] ="./Data/Input/Input File/"
-app.config["INPUT_AUDIO_FOLDER"] = ".\\Data\\Input\\Input Audio\\"
-app.config["INPUT_PDF_FOLDER"] = ".\\Data\\Input\\Input File\\"
-app.config["INPUT_CHUNKS_FOLDER"] = ".\\Data\\Input\\Input Chunks\\"
-app.config['TRANSCRIPTED_AUDIO_FOLDER'] = ".\\Data\\Output\\Audio Transcript\\"
-# app.config["GPT_RESPONSE"] ='.\\recordings\\Output\\GPT Response\\'
-# app.config["OUTPUT_AUDIO"] = '.\\recordings\\Output\\Output Audio\\'
-# app.config['STATIC_PATH']= '.\\static\\'
+app.config["UPLOAD_AUDIO_FOLDER"] = os.getenv('UPLOAD_AUDIO_FOLDER')
+app.config["UPLOAD_PDF_FOLDER"] = os.getenv('UPLOAD_PDF_FOLDER')
+app.config["UPLOAD_EXCEL_FOLDER"] = os.getenv('UPLOAD_EXCEL_FOLDER')
+app.config["INPUT_AUDIO_FOLDER"] = os.path.join(os.getcwd(),'Data','Input','Input Audio')
+app.config["INPUT_PDF_FOLDER"] = os.path.join(os.getcwd(),'Data','Input','Input File')
+app.config["VECTOR_DB_FOLDER"] = os.path.join(os.getcwd(),'Data','Input','VectorDB')
+app.config["INPUT_CHUNKS_FOLDER"] = os.path.join(os.getcwd(),'Data','Input','Input Chunks')
+app.config['TRANSCRIPTED_AUDIO_FOLDER'] = os.path.join(os.getcwd(),'Data','Output','Audio Transcript')
+
+data_obj = Data(app.config["INPUT_PDF_FOLDER"], app.config["VECTOR_DB_FOLDER"])
+model_obj = Model()
 
 @app.route('/', methods=['GET','POST'])
 @app.route('/home',methods=['GET','POST'])
 def audioTranscripter():
+    # for pdf_filename in os.listdir(app.config["UPLOAD_PDF_FOLDER"]):
+    #     pdf_path = os.path.join(app.config["UPLOAD_PDF_FOLDER"],pdf_filename)
+    #     os.remove(pdf_path)
+
+    if len(os.listdir(app.config["VECTOR_DB_FOLDER"]))>0:
+        for vector_filename in os.listdir(app.config["VECTOR_DB_FOLDER"]):
+            vector_file = os.path.join(app.config["VECTOR_DB_FOLDER"],vector_filename)
+            os.remove(vector_file)
+
+    if len(os.listdir(app.config['TRANSCRIPTED_AUDIO_FOLDER']))>0:
+        for transcript_filename in os.listdir(app.config['TRANSCRIPTED_AUDIO_FOLDER']):
+            transcription = os.path.join(app.config['TRANSCRIPTED_AUDIO_FOLDER'],transcript_filename)
+            os.remove(transcription)
+                
+
     return render_template('index.html')
 
 @app.route('/result', methods=['GET','POST'])
 def result():
     if request.method == "POST":
-        print(request.files)
         if 'data' in request.files:
             # input_audio_file = 'recorded_audio.wav'
             # os.remove(os.path.join(os.path.join(app.config['INPUT_AUDIO'],input_audio_file)))
@@ -52,8 +67,8 @@ def result():
             filepath = os.path.join(app.config["UPLOAD_AUDIO_FOLDER"] , 'recorded_audio.wav')
             file.save(filepath)
             speech2Text(app.config["INPUT_AUDIO_FOLDER"] , app.config['TRANSCRIPTED_AUDIO_FOLDER'])
-
-            return jsonify("Input Audio is stored")
+            if len(os.listdir(app.config['TRANSCRIPTED_AUDIO_FOLDER']))>0:
+                return jsonify("Input Audio is stored")
         
 
         if 'pdfFile' in request.files:
@@ -61,15 +76,10 @@ def result():
             file = request.files['pdfFile']
             filepath = os.path.join(app.config["UPLOAD_PDF_FOLDER"], pdf_filename)
             file.save(filepath)
-            _chunkifyPdfFile(app.config["INPUT_PDF_FOLDER"] , app.config["INPUT_CHUNKS_FOLDER"], pdf_filename)
-
-            return jsonify("Input PDF is stored")
-
-            # transcripted_text = 'input_audio_transcription.txt'
-            # # os.remove(os.path.join(app.config['INPUT_AUDIO_TRANSCRIPTION'],transcripted_text))
-            # response_text = generateResponse(app.config['INPUT_AUDIO_TRANSCRIPTION'], app.config["GPT_RESPONSE"] )
-
-            # return jsonify(response_text)
+            data_obj.createPDFVectorDBwithFAISS(chunk_size=2000, chunk_overlap=500)
+            if len(os.listdir(app.config["VECTOR_DB_FOLDER"]))>0:
+                return jsonify("Input PDF is stored")
+        
         if 'excelFile' in request.files:
             excel_filename = 'downloaded_excel.xlsx'
             file = request.files['excelFile']
@@ -77,36 +87,25 @@ def result():
             file.save(filepath)
 
             return jsonify("Input Excel is stored")
-
-
-
         else:
             error = 'Some Error Occured!'
             return jsonify({'error': error})
         
 @app.route('/text-question', methods=['GET','POST'])
 def fetchTextQuestion():
-    input_text = request.json['text'].strip()
+    input_question = request.json['text'].strip()
     input_file_path = app.config["INPUT_PDF_FOLDER"]
     files_dir = os.path.join(input_file_path,'*')
     latest_file = sorted(glob.iglob(files_dir), key=os.path.getmtime, reverse=True)[0]
     print(latest_file)
 
     if latest_file.count('.pdf') ==1:
-        with open(os.path.join(app.config["INPUT_CHUNKS_FOLDER"], 'chunks.json'), 'r') as f_read:
-            chunk_data = json.load(f_read)
-        f_read.close()
+        top_k_chunks = data_obj.create_top_k_chunk_from_FAISS(input_question, top_k =3)
 
-        # pdf_bot = Model(OPENAI_KEY, 
-        #                   chunk_data, 
-        #                   input_text) 
-        # print("\n OpenAI Generating results ... \n")
-        # response = pdf_bot.generateAnswer() 
-        # print("\n results generated \n")
-        return jsonify("HI from KD, Pdf "+ latest_file)
-        # return jsonify({'response': response})
-        # return jsonify(f"Chunk data Length : {len(chunk_data)}")
-        # return jsonify({"status": 200, 'message':'success'})
+        prompt = model_obj.createQuestionPrompt(top_k_chunks)
+        prompt_template = model_obj.createQuestionPromptTemplate(prompt)
+        response = model_obj.generateAnswerwithMemory(input_question, prompt_template,chat_history=[])
+        return jsonify(response),200
 
     if latest_file.count('.xlsx') == 1:
         # stored_excel_filename = 'downloaded_excel.xlsx'
@@ -114,7 +113,7 @@ def fetchTextQuestion():
         # query_result = ExcelQuery(OPENAI_KEY).excelQuery(latest_file,input_text)
         # return jsonify(query_result)
 
-        
+        time.sleep(5)
         return jsonify("HI from KD, Excel " + latest_file)
 
 
@@ -123,51 +122,15 @@ def fetchTextQuestion():
 @app.route('/audio-question', methods=['GET','POST'])
 def fetchAudioQuestion():
     transcripted_audio_file = 'input_audio_transcription.txt'
-
-    with open(os.path.join(app.config["INPUT_PDF_FOLDER"], 'chunks.json'), 'r') as f_read:
-        chunk_data = json.load(f_read)
-    f_read.close()
-    
     with open(os.path.join(app.config['TRANSCRIPTED_AUDIO_FOLDER'], transcripted_audio_file)) as f_audio:
-        input_text = "".join(f_audio)
+        input_question = "".join(f_audio)
     f_audio.close()
-
-    print("Transcripted Input Audio : ", input_text) 
-
-    # pdf_bot = Model(OPENAI_KEY, 
-    #                   chunk_data, 
-    #                   input_text) 
-    # print("\n OpenAI Generating results ... \n")
-    # response = pdf_bot.generateAnswer() 
-    # print("\n results generated \n")
-    return jsonify("HI from KD")
-
-
-
-# @app.route('/display', methods=['GET','POST'])
-# def displayText():
-#     if request.method == "POST":
-#         gpt_response_text = 'response.txt'
-#         print(app.config["INPUT_AUDIO"])
-#         # os.remove(os.path.join(os.path.join(app.config["GPT_RESPONSE"] ,gpt_response_text)))
-#         transcript = speech2Text(app.config["INPUT_AUDIO"], app.config['INPUT_AUDIO_TRANSCRIPTION'])
-#         return jsonify(transcript)
-
-# @app.route('/audate',methods=['GET','POST'])
-# def audateResponse():
-#     if request.method == "POST":
-#         response_text = generateResponse(app.config['INPUT_AUDIO_TRANSCRIPTION'], app.config["GPT_RESPONSE"] )
-#         synthesize_speech(app.config["GPT_RESPONSE"],app.config["OUTPUT_AUDIO"])
-#         return jsonify(response_text)
-
-
-def _chunkifyPdfFile(pdf_filepath,chunk_filepath, filename):
-    pdf_path = os.path.join(pdf_filepath,filename)
-    chunks = Data(pdf_path).text_to_chunk()
-    with open(os.path.join(chunk_filepath,'chunks.json'),'w') as f:
-        json.dump(chunks, f)
-    f.close()
-
+    top_k_chunks = data_obj.create_top_k_chunk_from_FAISS(input_question, top_k =3)
+    prompt = model_obj.createQuestionPrompt(top_k_chunks)
+    prompt_template = model_obj.createQuestionPromptTemplate(prompt)
+    response = model_obj.generateAnswerwithMemory(input_question, prompt_template,chat_history=[])
+    print(response)
+    return jsonify(response)
 
 
 if __name__ == "__main__":
